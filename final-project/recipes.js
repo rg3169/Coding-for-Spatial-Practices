9af16399527cb49ffffd3152370715f7d4965e4f
rg3169 <rg3169@columbia.edu>
2021-04-26 21:59:05 -0400

    1

---
+++ final-project/recipes.js
@@ -0,0 +1,40 @@
+const recipes = [
+
+{
+  id: 1, 
+  name: "ButterChicken",
+  ingredients: ["chicken", "ghee", "cumin", "green chillies", "onions", "tomatoes", "turmeric"]
+};
+{  
+  id: 2,
+  name: "StuffedParantha"
+  ingredients: ["ghee", "green chillies", "onions", "cumin", "butter"]
+};
+{
+  id: 3,
+  name: "RailwayLambCurry"  
+  ingredients: ["turmeric", "lamb", "curry leaves", "onion", "potato", "cumin"]
+};
+{
+  id: 4,
+  name: "Sambar"  
+  ingredients: ["turmeric", "asefoetida", "curry leaves", "onions", "tomatoes"]
+};
+{
+  id: 5,
+  name: "MalabarPrawns"  
+  ingredients: ["turmeric", "curry leaves", "green chillies", "onions", "prawns"]
+};
+  
+  ];
+
+const recipe_container = document.querySelector(".flex-grid");
+
+
+
+
+
+
+
+
+
